c1eeab665dd596edfba6d19037ad13baa3a9f733
wuth01 <784709808@qq.com>
2020-10-26 17:01:37 +0800

    2020.10.25作业

--- web/workweixin/testcase/test_import_member.py
+++ web/workweixin/testcase/test_import_member.py
@@ -5,14 +5,13 @@ class TestWX:
 
     def test_imort_success(self):
         # assert self.index.goto_login().goto_register().register()
-        text = self.login.logincookies().goto_TestImport().import_success()
+        text = self.login.logincookies().goto_TestImport().import_success(filename="",text="")
         assert "导入成功1人"== text
 
     def test_import_fail1(self):
-        text =self.login.logincookies().goto_TestImport().import_fail1()
+        text =self.login.logincookies().goto_TestImport().import_fail1(filename="",text="")
         assert "批量导入模板错误" == text
 
     def test_import_fail2(self):
         text=self.login.logincookies().goto_TestImport().import_fail2(filename="",text="")
         assert "无变化1人" == text
-
